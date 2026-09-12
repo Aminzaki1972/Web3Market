75e8910e46efe95b576e1f8dfb0f1ea2e481b96a
Amin Zaki mohamed <101603364+Aminzaki1972@users.noreply.github.com>
2026-09-12 18:31:29 +0300

    Use session-scoped Buyer offer return state to prevent stale redirects

--- js/buyer-offer-flow.js
+++ js/buyer-offer-flow.js
@@ -3,7 +3,7 @@
  const id=new URLSearchParams(location.search).get('id');
  if(!id)return;
  const returnTo='project.html?id='+encodeURIComponent(id);
- const save=()=>{try{localStorage.setItem('web3market_offer_return_to',returnTo)}catch(_) {}};
+ const save=()=>{try{sessionStorage.setItem('web3market_offer_return_to',returnTo);sessionStorage.setItem('web3market_offer_flow','1')}catch(_) {}};
  document.addEventListener('click',e=>{
   const a=e.target.closest?.('a[href*="register.html?role=buyer"]');
   if(a)save();
